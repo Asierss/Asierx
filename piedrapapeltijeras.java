f83942ae9412cb5153f1bbb2a22dba2118a76b55
Asier <asierouekkai@paucasesnoves.cifp.cat>
2020-02-19 12:05:29 +0100

    PPTLS 1

--- piedrapapeltijeras.java
+++ piedrapapeltijeras.java
@@ -2,7 +2,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class piedrapapeltijeraslagartospock {
-    //variables globals que necessitarem per poder correr el programa.
+    //variables globals que necessitarem per poder correr el progrwfijnwrqriufnwuinfuiqwrama.
     public static int idioma;
     public static int contador=1;
     public static int tiradaUser=0;
